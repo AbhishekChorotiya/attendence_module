028becd7cc372c15016101f49ed51ae10add01c5
AbhishekChorotiya <2020kuec2033@iiitkota.ac.in>
2023-10-12 02:21:42 +0530

    Added Geo Coding

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,22 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
+var cors = require("cors");
 
-app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 );
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+  );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   const ip =
@@ -22,10 +28,20 @@ app.get("/", (req, res) => {
     req.socket.remoteAddress ||
     "";
 
-    res.json({"ip":ip})
+    res.json(ip)
+});
+app.post("/find", (req, res) => {
+  const ip =
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress ||
+    "";
+
+    res.json(ip)
 });
 
 
 app.listen(port,()=>{
-    console.log('listing on 3000')
+    console.log('listing on 5000')
 })
--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const [latitude,setLatitude] = useState(0);
+  const [longitude,setLongitude] = useState(0);
+  const [ip,setIP] = useState('');
+
+  useEffect(()=>{
+
+    const success = (pos)=>{
+      setLatitude(Math.floor(pos.coords.latitude*1000))
+      setLongitude(Math.floor(pos.coords.longitude*1000))
+      console.log(Math.floor(pos.coords.latitude*1000),Math.floor(pos.coords.longitude*1000))
+    }
+  
+    const err = () =>{
+      console.log("error")
+    }
+  
+    navigator.geolocation.getCurrentPosition(success,err);
+
+  },[])
+
+
+
+  const handleSubmit = async ()=>{
+
+    const post_data = {
+      latitude,longitude
+    }
+
+    const res = await fetch('http://localhost:5000/find',{
+      method:'post',
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(post_data)
+    })
+
+    const data = await res.json()
+    console.log(data)
+    // alert(data)
+    setIP(data);
+
+
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing Geocoding and IP
         </a>
+        <h1>{`Latitude: ${latitude}`}</h1>
+        <h1>{`longitude: ${longitude}`}</h1>
+        <h1>{`IP: ${ip}`}</h1>
+      <button onClick={handleSubmit}>Get my geoCode</button>
       </header>
     </div>
   );
